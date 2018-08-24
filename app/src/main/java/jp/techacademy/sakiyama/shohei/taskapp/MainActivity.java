3111e94e5b5090d3850d0761e54e3b8baff4a36c
sakiyama-s <sakiyama.s-hu@nhk.or.jp>
2018-08-24 18:25:57 +0900

    カテゴリ検索機能追加

--- app/src/main/java/jp/techacademy/sakiyama/shohei/taskapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/sakiyama/shohei/taskapp/MainActivity.java
@@ -1,52 +1,175 @@
 package jp.techacademy.sakiyama.shohei.taskapp;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import io.realm.Realm;
+import io.realm.RealmChangeListener;
+import io.realm.RealmResults;
+import io.realm.Sort;
+
 
 public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_TASK = "jp.techacademy.sakiyama.shohei.taskapp.TASK";
+
+    private Realm mRealm;
+    private RealmChangeListener mRealmListener = new RealmChangeListener() {
+        @Override
+        public void onChange(Object element) {
+            reloadListView();
+        }
+    };
+    private ListView mListView;
+    private TaskAdapter mTaskAdapter;
+    private Button mCategoryButton;
+    private EditText mCategorySearchText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, InputActivity.class);
+                startActivity(intent);
             }
         });
+
+        // Realmの設定
+        mRealm = Realm.getDefaultInstance();
+        mRealm.addChangeListener(mRealmListener);
+
+        // ListViewの設定
+        mTaskAdapter = new TaskAdapter(MainActivity.this);
+        mListView = (ListView) findViewById(R.id.listView1);
+
+        // Buttonの設定
+        mCategoryButton = (Button) findViewById(R.id.searchCategory_button);
+
+        // EditText
+        mCategorySearchText = (EditText) findViewById(R.id.editText_searchCategory);
+
+        // カテゴリ検索ボタンが押された時の処理
+        mCategoryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (!mCategorySearchText.getText().toString().equals("")) {
+                    // editTextに何か入力されている時はヒットしたタスクのみを表示
+                    searchCategoryListView(mCategorySearchText.getText().toString());
+                } else {
+                    // editTextが空でボタンを押下された時は、タスクをすべて表示
+                    reloadListView();
+                }
+
+            }
+        });
+
+
+        // ListViewをタップしたときの処理
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // 入力・編集する画面に遷移させる
+                Task task = (Task) parent.getAdapter().getItem(position);
+
+                Intent intent = new Intent(MainActivity.this, InputActivity.class);
+                intent.putExtra(EXTRA_TASK, task.getId());
+
+                startActivity(intent);
+            }
+        });
+
+        // ListViewを長押ししたときの処理
+        mListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                // タスクを削除する
+
+                final Task task = (Task) parent.getAdapter().getItem(position);
+
+                // ダイアログを表示する
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                builder.setTitle("削除");
+                builder.setMessage(task.getTitle() + "を削除しますか");
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        RealmResults<Task> results = mRealm.where(Task.class).equalTo("id", task.getId()).findAll();
+
+                        mRealm.beginTransaction();
+                        results.deleteAllFromRealm();
+                        mRealm.commitTransaction();
+
+                        Intent resultIntent = new Intent(getApplicationContext(), TaskAlarmReceiver.class);
+                        PendingIntent resultPendingIntent = PendingIntent.getBroadcast(
+                                MainActivity.this,
+                                task.getId(),
+                                resultIntent,
+                                PendingIntent.FLAG_UPDATE_CURRENT
+                        );
+
+                        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+                        alarmManager.cancel(resultPendingIntent);
+
+                        reloadListView();
+                    }
+                });
+                builder.setNegativeButton("CANCEL", null);
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+                return true;
+            }
+        });
+
+        reloadListView();
+    }
+
+    private void searchCategoryListView(String keyword) {
+        RealmResults<Task> taskRealmResults = mRealm.where(Task.class)
+                .equalTo("category", keyword)
+                .findAll()
+                .sort("date", Sort.DESCENDING);
+
+        mTaskAdapter.setTaskList(mRealm.copyFromRealm(taskRealmResults));
+        mListView.setAdapter(mTaskAdapter);
+        mTaskAdapter.notifyDataSetChanged();
+    }
+
+    private void reloadListView() {
+        // Realmデータベースから、「全てのデータを取得して新しい日時順に並べた結果」を取得
+        RealmResults<Task> taskRealmResults = mRealm.where(Task.class).findAll().sort("date", Sort.DESCENDING);
+        // 上記の結果を、TaskList としてセットする
+        mTaskAdapter.setTaskList(mRealm.copyFromRealm(taskRealmResults));
+        // TaskのListView用のアダプタに渡す
+        mListView.setAdapter(mTaskAdapter);
+        // 表示を更新するために、アダプターにデータが変更されたことを知らせる
+        mTaskAdapter.notifyDataSetChanged();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    protected void onDestroy() {
+        super.onDestroy();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        mRealm.close();
     }
 }
